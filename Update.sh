a41c88fafe18a037627b9702eba11b854fa716a8
ebears <80941110+ebears@users.noreply.github.com>
2023-12-13 15:02:17 -0500

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# placeholders
+# Definitions
 FLATPAK_APP_NAME="io.github.srb2.rphys"
 ZIP_DOWNLOAD_LINK="https://github.com/ebears/SRB2-Steam-Deck-Custom/raw/main/SRB2-XZPY-V1.3-SD.zip"
 ZIP_DOWNLOAD_NAME="SRB2-XZPY-V1.3-SD.zip"
 ZIP_FOLDER_NAME="SRB2-XZPY-V1.3-SD"
 EXTRACTION_LOCATION="$HOME/.var/app/io.github.srb2.rphys/.srb2"
 
-# Install the Flatpak application
+# Install SRB2rphys Flatpak
 echo "Installing $FLATPAK_APP_NAME via Flatpak..."
 flatpak install --user $FLATPAK_APP_NAME -y --noninteractive
 
-# Download the zip file
-echo "Downloading server custom content and models..."
+# Download zip file
+echo "Downloading custom content and models..."
 wget $ZIP_DOWNLOAD_LINK -O $ZIP_DOWNLOAD_NAME
 
-# Check if the extraction directory exists, and create it if necessary
+# Check if the extraction directory exists, create it if necessary
+echo Checking if $EXTRACTION_LOCATION exists..."
 if [ ! -d "$EXTRACTION_LOCATION" ]; then
   echo "Creating directory: $EXTRACTION_LOCATION"
   mkdir -p "$EXTRACTION_LOCATION"
+else
+  echo "$EXTRACTIONLOCATION already exists, skipping...
 fi
 
 # Create a temporary directory for extraction
@@ -32,7 +35,7 @@ unzip -o $ZIP_DOWNLOAD_NAME -d $temp_dir
 mv -f $temp_dir/$ZIP_FOLDER_NAME/* $EXTRACTION_LOCATION
 
 # Clean up the temporary directory
-rm -r $temp_dir/$ZIP_FOLDER_NAME
+rm -r $temp_dir
 
 # Clean up the downloaded zip file
 echo "Cleaning up..."
